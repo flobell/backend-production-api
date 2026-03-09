98759537451fda483b99732ed77a42bcf597a15d
Pedro Flores <manuelflores1795@gmail.com>
2026-03-09 16:40:03 -0500

    feat: add timestamps to user model

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime, timezone
 from app.db.base import Base
 
 
@@ -8,3 +9,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
