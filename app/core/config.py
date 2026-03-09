c0bb7da0006b1b080eb5d1eb4e093c77fbfe1219
Pedro Flores <manuelflores1795@gmail.com>
2026-03-09 16:33:46 -0500

    refactor: move configuration to environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,9 @@
-SECRET_KEY = "secret_key_here"  # TODO: move this to an environment variable (.env)
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60  # 1 hour
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
