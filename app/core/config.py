84d4273f45dfd23f40a2dbc13cb8089ce91acf5d
Pedro Flores <manuelflores1795@gmail.com>
2026-03-10 16:18:09 -0500

    clean: cleaning errors

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "")
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY", "thisisaverysecretkey123")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
